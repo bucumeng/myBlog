be6c4f917db4aa1238ac14741ff8553a7aecfb7d
burcu <burcumenguc06@gmail.com>
2024-06-23 17:44:27 +0300

    first commit

---
+++ public/js/search.js
@@ -0,0 +1,6 @@
+const searhIcon = document.getElementById('searchIcon')
+const searhEnginer = document.getElementById('searhEnginer')
+
+searhIcon.addEventListener('click', ()=>{
+searhEnginer.classList.toggle('active')
+})
